1093ef308d0f43bad716da54619f6069e9d73d38
Andrew Sullivan <licyeus@gmail.com>
2016-03-10 12:11:30 -0800

    Refactor

--- tasks/parse-gtfs.js
+++ tasks/parse-gtfs.js
@@ -14,20 +14,45 @@ function parseGtfsFile (filename) {
   })
 }
 
-const loadRoute = shortName => parseGtfsFile('routes').then(data => _.find(data, { route_short_name: shortName }))
-const loadTripsForRoute = routeId => parseGtfsFile('trips').then(data => _.filter(data, { route_id: routeId }))
-const loadTripsForBlock = blockId => parseGtfsFile('trips').then(data => _.filter(data, { block_id: blockId }))
-const loadShape = shapeId => parseGtfsFile('shapes').then(data => _.filter(data, { shape_id: shapeId }))
 
-co(function *() {
-  const route = yield loadRoute('26')
-  const trips = yield loadTripsForRoute(route.route_id)
-  const blockIds = _.chain(trips).map('block_id').uniq().value()
-  const blockTrips = yield loadTripsForBlock(blockIds[0])
-  const shapeIds = _.chain(blockTrips).filter({ direction_id: '1' }).map('shape_id').uniq().value()
-  const shape = yield loadShape(shapeIds[0])
-  const points = _.reduce(shape, (acc, v) => acc.concat([[v.shape_pt_lon, v.shape_pt_lat]]), [])
-  const geojson = GeoJSON.parse([{ line: points }], { LineString: 'line' })
-  console.log(JSON.stringify(geojson))
-})
+// const loadRoute = shortName => parseGtfsFile('routes').then(data => _.find(data, { route_short_name: shortName }))
+// const loadTripsForRoute = routeId => parseGtfsFile('trips').then(data => _.filter(data, { route_id: routeId }))
+// const loadTripsForBlock = blockId => parseGtfsFile('trips').then(data => _.filter(data, { block_id: blockId }))
+// const loadShape = shapeId => parseGtfsFile('shapes').then(data => _.filter(data, { shape_id: shapeId }))
+
+// co(function *() {
+//   const route = yield loadRoute('26')
+//   const trips = yield loadTripsForRoute(route.route_id)
+//   const blockIds = _.chain(trips).map('block_id').uniq().value()
+//   const blockTrips = yield loadTripsForBlock(blockIds[0])
+//   const shapeIds = _.chain(blockTrips).filter({ direction_id: '1' }).map('shape_id').uniq().value()
+//   const shape = yield loadShape(shapeIds[0])
+//   const points = _.reduce(shape, (acc, v) => acc.concat([[v.shape_pt_lon, v.shape_pt_lat]]), [])
+//   const geojson = GeoJSON.parse([{ line: points }], { LineString: 'line' })
+//   console.log(JSON.stringify(geojson))
+// })
+
+const extractShapes = function *(cb) {
+  const shapes = yield parseGtfsFile('shapes')
+  const uniqueShapeIds = _.chain(shapes).map('shape_id').uniq().value()
+
+  uniqueShapeIds.slice(0,3).forEach(shapeId => {
+    const shape = _.filter(shapes, { shape_id: shapeId })
+    const points = _.reduce(shape, (acc, v) => acc.concat([[v.shape_pt_lon, v.shape_pt_lat]]), [])
+    const geojson = GeoJSON.parse([{ line: points }], { LineString: 'line' })
+    cb(shapeId, geojson)
+  })
+}
+
+const shapesForRouteId = function *(routeId) {
+  const trips = yield parseGtfsFile('trips')
+  const shapeIdsForRoute = _.chain(trips).filter({ route_id: routeId }).map('shape_id').uniq().value()
+  console.log(shapeIdsForRoute.map(id => `./shapes/${id}.geojson`).join(' '))
+}
+
+co(extractShapes((shapeId, geojson) => {
+  fs.writeFileSync(`./shapes/${shapeId}.geojson`, JSON.stringify(geojson), 'utf8')
+}))
+
+// co(shapesForRouteId('100151'))
 
